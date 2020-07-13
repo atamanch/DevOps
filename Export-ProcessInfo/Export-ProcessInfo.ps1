26599d932393cc24f753ea05b7e463aa7dccd13f
atamanch <atamanch@gmail.com>
2020-07-13 15:46:37 +0800

    Add a memory threshold parameter and user friendly status logging.

--- Export-ProcessInfo/Export-ProcessInfo.ps1
+++ Export-ProcessInfo/Export-ProcessInfo.ps1
@@ -2,10 +2,16 @@
 
 # Feel free to uncomment lines with variables ending in "GP" if you prefer to use Get-Process instead of Get-WmiObject
 
-# Create timestamp variable
-$Date = Get-Date -Format "MMddyyyy"
+# Enter the name of the process you would like to monitor
 $ProcessName = "chrome.exe"
 # $ProcessNameGP = "chrome"
+
+# Enter optional threshold for process memory working set usage in Megabytes. Processes that do not meet this value will be ignored.
+$processMemoryThreshold = 60
+
+# Create timestamp variable
+$Date = Get-Date -Format "MMddyyyy"
+
 
 # List machines here
 $machines = @(
@@ -30,21 +36,22 @@ ForEach ($machine in $machines) {
 
     # Special case where Get-Process doesn't work well with localhost
     ElseIf ($machine = "localhost") {
-        # Grab the machine info if it successfully pings
-        $chromeInfo = Get-WmiObject -Class Win32_Process -ComputerName $machine | Where-Object {$_.Name -like $ProcessName} | Select-Object PSComputerName, ProcessName, @{Name="Mem Usage(MB)";Expression={[math]::round($_.ws / 1mb)}}, ProcessId, CommandLine
-        # $chromeInfoGP = Get-Process -Name $ProcessNameGP -IncludeUserName | Select UserName, Name, @{Name="Mem Usage(MB)";Expression={[math]::round($_.ws / 1mb)}}, Id
-
+        # Grab the local machine info
+        $chromeInfo = Get-WmiObject -Class Win32_Process -ComputerName $machine | Where-Object {($_.Name -like $ProcessName) -and (($_.ws / 1mb) -gt $processMemoryThreshold)} | Select-Object PSComputerName, ProcessName, @{Name="Mem Usage(MB)";Expression={[math]::round($_.ws / 1mb)}}, ProcessId, CommandLine
         $allChromeInfo += $chromeInfo
+
+        # $chromeInfoGP = Get-Process -Name $ProcessNameGP -IncludeUserName | Select UserName, Name, @{Name="Mem Usage(MB)";Expression={[math]::round($_.ws / 1mb)}}, Id
         # $allChromeInfoGP += $chromeInfoGP
     }
     
     # Grab the remote machine info if it successfully pings
     Else {
-        $chromeInfo = Get-WmiObject -Class Win32_Process -ComputerName $machine | Where-Object {$_.Name -like $ProcessName} | Select-Object PSComputerName, ProcessName, @{Name="Mem Usage(MB)";Expression={[math]::round($_.ws / 1mb)}}, ProcessId, CommandLine
-        # $chromeInfoGP = Get-Process -Name $ProcessNameGP -ComputerName $machine -IncludeUserName | Select UserName, Name, @{Name="Mem Usage(MB)";Expression={[math]::round($_.ws / 1mb)}}, Id
-
+        $chromeInfo = Get-WmiObject -Class Win32_Process -ComputerName $machine | Where-Object {($_.Name -like $ProcessName) -and (($_.ws / 1mb) -gt $processMemoryThreshold)} | Select-Object PSComputerName, ProcessName, @{Name="Mem Usage(MB)";Expression={[math]::round($_.ws / 1mb)}}, ProcessId, CommandLine
         $allChromeInfo += $chromeInfo
+        
+        # $chromeInfoGP = Get-Process -Name $ProcessNameGP -ComputerName $machine -IncludeUserName | Select UserName, Name, @{Name="Mem Usage(MB)";Expression={[math]::round($_.ws / 1mb)}}, Id
         # $allChromeInfoGP += $chromeInfoGP
+        
     }
 
 }
@@ -56,3 +63,5 @@ $allChromeInfo | Format-Table -AutoSize
 # Export result into CSV format
 $allChromeInfo  | Export-Csv -NoType $PSScriptRoot\ChromeProcessTable-$Date.csv -append
 # $allChromeInfoGP  | Export-Csv -NoType $PSScriptRoot\ChromeProcessTableGP-$Date.csv -append
+
+Write-Output "Process export complete, you can find it at $PSScriptRoot\ChromeProcessTable-$Date.csv"
